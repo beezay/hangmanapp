007af537feb7a564a6b19db64228e3fec149c9e3
Beezay Bohora <bijay.okur@gmail.com>
2020-08-09 21:57:30 +0545

    Hangman react app

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div>
+            <h1>HANGMAN Game</h1>
+            <p>This is Hangman Game and You got 8 chances to win this Game</p>
+            <p>Please enter the characters</p>
+        </div>
+    )
+}
+
+export default Header
